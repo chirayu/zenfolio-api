19340e9c1e9843d21af859f3e30e0a1bfde90d7a
chirayu@chirayu.org <chirayu@chirayu.org@b05c2538-c247-0410-882a-9dee131e08a9>
2008-03-10 10:50:00 +0000

    added logic to detect if the gallery exists. It works but is not foolproof

--- code/zenfolioapi/smug2zen.py
+++ code/zenfolioapi/smug2zen.py
@@ -7,7 +7,7 @@ import urllib2
 
 def init_parser():
         
-    parser = OptionParser (usage="%prog [--smuglogin login] [--smugpasswpord password] [--zenlogin login] [--zenpassword password] [--smuggallery gallery]")
+    parser = OptionParser (usage="%prog [--smuglogin login] [--smugpasswpord password] [--zenlogin login] [--zenpassword password] ")
     
     parser.add_option("--smuglogin",
                       action="store", type="string", dest="smug_login",
@@ -24,10 +24,6 @@ def init_parser():
     parser.add_option("--zenpassword",
                       action="store", type="string", dest="zen_password",
                       help="Zenfolio Password")
-
-    parser.add_option("--smugalbum",
-                      action="store", type="string", dest="smug_album",
-                      help="The smugmug album that has to be transferred")
 
     return parser
 
@@ -56,7 +52,7 @@ def s_download_image (sapi, session_id, image_id, image_key):
     headers['X-Smug-SessionID'] = session_id
 
     req = urllib2.Request(original_url, headers=headers)
-    opener = urllib2.build_opener(urllib2.HTTPHandler(debuglevel=1))    
+    opener = urllib2.build_opener(urllib2.HTTPHandler(debuglevel=0))    
     try:
         data = opener.open(req).read()
     except Exception, e:
@@ -74,23 +70,58 @@ def transfer_image_s2z (sapi, session_id, image_id, image_key, zapi, upload_path
 def transfer_albums (sapi, session_id, zapi):
     """ download a complete album """
 
+    user_status = None
+
     # Get the location of the Zenfolio photo sets
     result=zapi.LoadGroupHierarchy ("cpandgp")
-    root_zenfolio_id = result["result"]["Elements"][0]["Id"]
+    root_zenfolio_id = result["result"]["Elements"][0]["Id"] # TBD : should come from command line
+    root_element = result["result"]["Elements"][0]
     print "Loading the group hierarchy"
-
-
+    
     # Step 1: fetch all albums
     result = sapi.albums_get(SessionID=session_id, Heavy = True) # TBD - Heavy does not seem to work
     album_list = result.Albums[0].Album
+    album_set = set()
+    for album in album_list: album_set.add (album["Title"])
+
+    if len (album_set) != len (album_list):
+        print "There are some duplicate album names. Please rename accordingly"
+    
+    
     for album in album_list:
         album_id = album["id"]
         album_title = album["Title"]
+        # Step 3: Check if the album is present in zenfolio
+        matches = []
+        for element in root_element["Elements"]:
+            if element ["Type"] == "Gallery" and element["$type"] == "PhotoSet":
+                if element ["Title"] == album_title:
+                    matches.append (element)
+
+        if len (matches) == 0:
+            # Step 4: Create a zenfolio gallery
+            print "Creating Zenfolio Gallery : %s" % album_title
+            zen_photoset_id, zen_upload_path = z_create_gallery (zapi, root_zenfolio_id, album_title, album_description)
+        elif len (matches) == 1:
+            zen_photoset_id, zen_upload_path = matches[0]["Id"], matches[0]["UploadUrl"] # TBD: what about multiple matches
+        else:
+            print "Multiple albums with the name %s exist in Zenfolio. am using the first album to transfer all images" % album_title
+            zen_photoset_id, zen_upload_path = matches[0]["Id"], matches[0]["UploadUrl"] # TBD: what about multiple matches
+        
+        
+        # Get album details
         result = sapi.albums_getInfo(SessionID=session_id, AlbumID=album_id)
         album_description = result.Album[0]["Description"]
+        
+        album_image_count = result.Album[0]["ImageCount"]
+
+        if matches and album_image_count == matches[0]["PhotoCount"]:
+            continue # complete album is most likely updated. Ideally a check on 
+        
+        
         # Step 2: Ask the user if he wants to transfer the complete album
-        while True:
-            user_status=raw_input('Do you want to transfer album %s? (y(es)/n(o)/a(bort) : ' % album_title)
+        while True and user_status != 'D':
+            user_status=raw_input('Do you want to transfer album %s? (y(es)/n(o)/a(bort)/D(on\'t prompt) : ' % album_title)
             if not (user_status == 'y' or user_status == 'n' or user_status == 'a'):
                 print "Enter correct value."
                 continue
@@ -105,19 +136,16 @@ def transfer_albums (sapi, session_id, zapi):
             continue
 
         # Step 3: Check if the album exists in Zenfolio, with a similar number of pictures - TBD
-        # Step 4: Create a zenfolio gallery
-        print "Creating Zenfolio Gallery : %s" % album_title
-        zen_photoset_id, zen_upload_path = z_create_gallery (zapi, root_zenfolio_id, album_title, album_description)
 
         # Step 5: get all the imgaes (id)  from smugmug
         image_list = s_get_all_images (sapi, session_id, album_id)
 
         # Step 6: transfer images to zenfolio
         for image in image_list:
+            # TBD - there is no provision to sync partially synced albums
             if image.elementName == "Image":
-                import pdb
-                pdb.set_trace()
                 transfer_image_s2z (sapi, session_id, image["id"], image["Key"], zapi, zen_upload_path, zen_photoset_id)
+                print ".",
             # transfer the images to smugmug
 
         print "Transfer complete"
